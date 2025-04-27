1b350e16c4a522f8f1f005b61b7dc17b0bdf7c1d
ashley-rr <160376224+ashley-rr@users.noreply.github.com>
2025-04-26 22:54:39 -0700

    started working on landing page with React

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,8 +3,25 @@ import React from "react";
 export default function Page() {
   return (
     <>
-      <h1 className="text-purple-800">About</h1>
-      <p>Massa urna magnis dignissim id euismod porttitor vitae etiam viverra nunc at adipiscing sit morbi aliquet mauris porttitor nisi, senectus pharetra, ac porttitor orci.</p>
-    </>)
+      <div className="bg-sky-300 w-[50%] h-screen flex items-center justify-center">
+        <div className="rounded-[2vw] drop-shadow-lg bg-white text-center w-[70%] h-[40%]"> 
+          <p className="text-[2.5rem]">Click an icon to find nearby services.</p>
+          <img className="w-[10rem]" src="/public/clip-art-for-dinner-13.png"></img>
+          <div className="flex gap-8 items-center justify-center pt-[3rem] text-[2rem]">
+            <div className="rounded-[1vw] h-[15rem] w-[13rem] bg-gray-100">
+            hi
+            </div>
+            <div className="rounded-[1vw] h-[15rem] w-[13rem] bg-gray-100">
+              
+            hi
+            </div>
+            <div className="rounded-[1vw] h-[15rem] w-[13rem] bg-gray-100">
+            hi
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 
 }
